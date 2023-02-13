842413d64ce5bdb1778efd315894244aaa011fc0
KristersRicardsAunins <125254175+KristersRicardsAunins@users.noreply.github.com>
2023-02-13 12:26:12 +0200

    test

--- main.py
+++ main.py
@@ -14,25 +14,28 @@ def find_mismatch(text):
     for i, next in enumerate(text):
         if next in "([{":
             # Process opening bracket, write your code here
-            opening_brackets_stack.append(next, i+1)
-            pass
+            opening_brackets_stack.append(Bracket(next, i+1))
+           # pass
 
         if next in ")]}":
             # Process closing bracket, write your code here
-            if not opening_brackets_stack or not opening_brackets_stack[-1].Matchc(next) :
+            if not opening_brackets_stack:
+                return i+1
+            if not are_matching(opening_brackets_stack[-1].char, next):
                 return i+1
             opening_brackets_stack.pop()
-            pass
+           # pass
+        if not opening_brackets_stack:
+            return "Success"
+        else:
+            return opening_brackets_stack[-1].position
 
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
     # Printing answer, write your code here
-if not opening_brackets_stack:
-    return 'Success'
-else:
-    return stack[-1].position
+print(result)
 
 if __name__ == "__main__":
     main()
