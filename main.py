cd2700234bd80aad1cc91b03a70aca8c4826ccbd
KristersRicardsAunins <125254175+KristersRicardsAunins@users.noreply.github.com>
2023-02-13 21:30:32 +0200

    test

--- main.py
+++ main.py
@@ -17,8 +17,10 @@ def find_mismatch(text):
 
         if next in ")]}":
             # Process closing bracket, write your code here
-            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
-                return i+1
+            if not opening_brackets_stack:
+                return i + 1
+            if not are_matching(opening_brackets_stack[-1].char, next):
+                return i + 1
             opening_brackets_stack.pop()
         if opening_brackets_stack:
             return opening_brackets_stack[0].position + 1
