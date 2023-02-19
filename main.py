8b58732ec46142c311ed1aab4cf3eb6f52582b99
KristersRicardsAunins <125254175+KristersRicardsAunins@users.noreply.github.com>
2023-02-19 19:32:57 +0200

    test

--- main.py
+++ main.py
@@ -1,34 +1,27 @@
 # python3
 
-from collections import namedtuple
-
-Bracket = namedtuple("Bracket", ["char", "position"])
-
-def are_matching(left, right):
-    return (left + right) in ["()", "[]", "{}"]
-
 def find_mismatch(text):
-    opening_brackets_stack = []
-    for i, next in enumerate(text):
-        if next in "([{":
-            opening_brackets_stack.append(Bracket(next, i))
-        if next in ")]}":
-            if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
+    opening_brackets = ['[', '{', '(']
+    closing_brackets = [']', '}', ')']
+    stack = []
+    for i, char in enumarate(text):
+        if char in opening_brackets:
+            stack.append((char, i))
+        elif char in closing_brackets:
+            if not stack:
                 return i + 1
-            opening_brackets_stack.pop()
-    if opening_brackets_stack:
-        return opening_brackets_stack[0].position + 1
-    return "Success"
-
-
+            last_opening_bracket, _ = stack.pop()
+            if (last_opening_bracket == '[' and char != ']') or (last_opening_bracket == '{' and char != '}') or (last_opening_bracket == '(' and char != ')'):
+                return i + 1
+            if stack:
+                _, index = stack.pop()
+                return index + 1
+            return "Success"
+        
 def main():
     text = input()
-    mismatch = find_mismatch(text)
-    # Printing answer, write your code here
-    if mismatch == "Success":
-        print("Success")
-    else:
-        print(mismatch)
-
+    for i in text:
+        print(find_mismatch(i))
+        
 if __name__ == "__main__":
     main()
