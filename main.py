adacd591466d8d0248a4f667bcd962e7e5e46e8f
KristersRicardsAunins <125254175+KristersRicardsAunins@users.noreply.github.com>
2023-02-19 22:59:07 +0200

    test

--- main.py
+++ main.py
@@ -10,39 +10,38 @@ def are_matching(left, right):
     return (left + right) in ["()", "[]", "{}"]
 
 
-def find_mismatch(iekavas):
+def find_mismatch(text):
     opening_brackets_stack = []
-    for i, next in enumerate(iekavas):
+    for i, next in enumerate(text):
         if next in "([{":
-            opening_brackets_stack.append(Bracket(next, i))
+            opening_brackets_stack.append(Bracket(next, i + 1))
         elif next in ")]}":
             if not opening_brackets_stack or not are_matching(opening_brackets_stack[-1].char, next):
                 return i + 1
             opening_brackets_stack.pop()
     if opening_brackets_stack:
-        return opening_brackets_stack[0].position + 1
+        return opening_brackets_stack[0].position
     return "Success"
 
 
 
 def main():
-    
     while True:
-        vai = input().strip().upper()
+        vai = input("I/F").strip().upper()
         if vai == "I":
-            iekavas = input()
+            text = input("Iekavas: ")
             break
         elif vai == "F":
-            fails = input()
+            fails = input("Fails: ")
             if os.path.exists(fails):
-                with open(fails) as i:
-                    iekavas = i.read()
+                with open(fails) as f:
+                    text = f.read()
                 break
             else:
                 print("wrong file")
         else:
             print("wrong input format")
-    mismatch = find_mismatch(iekavas)
+    mismatch = find_mismatch(text)
     # Printing answer, write your code here
     #if mismatch == "Success":
         #print("Success")
