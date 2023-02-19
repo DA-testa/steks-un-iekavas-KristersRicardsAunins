f43b7e4a0533306c63673900cba2a41c61275c7f
KristersRicardsAunins <125254175+KristersRicardsAunins@users.noreply.github.com>
2023-02-19 19:15:39 +0200

    test

--- main.py
+++ main.py
@@ -17,18 +17,26 @@ def find_mismatch(text):
                 return i + 1
             opening_brackets_stack.pop()
     if opening_brackets_stack:
-        return opening_brackets_stack[-1].position + 1
+        return opening_brackets_stack[0].position + 1
     return "Success"
 
 
 def main():
     text = input()
+    if text[-2:] == "IF":
+        text = text[:-2]
     mismatch = find_mismatch(text)
-    # Printing answer, write your code here
-    if mismatch == "Success":
+    
+    if mismatch == "Success"
         print("Success")
     else:
         print(mismatch)
+    #mismatch = find_mismatch(text)
+    # Printing answer, write your code here
+    #if mismatch == "Success":
+       # print("Success")
+   # else:
+       # print(mismatch)
 
 if __name__ == "__main__":
     main()
